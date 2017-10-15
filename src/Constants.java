a1690b385f71997f076099ea7b56db8b124cd92f
Jabir <thejabirmusa@gmail.com>
2017-10-15 21:07:56 +0300

    Added player and Alien sprite.

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,9 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
 
 
 public class Constants {
@@ -9,4 +14,23 @@ public class Constants {
     public static final int FRAME_WIDTH=500;
     public static final int GROUND=390;
     public static final Dimension FRAME_SIZE=new Dimension(Constants.FRAME_WIDTH,Constants.FRAME_HEIGHT);
+    public static BufferedImage[][] AlienSprites={{getSprites(0,0,22,16)},{getSprites(0,16,22,16)},
+                                                    {getSprites(22,0,16,16)},{getSprites(22,16,16,16)},
+                                                    {getSprites(22,0,16,16)},{getSprites(22,16,16,16)},
+                                                    {getSprites(38,0,24,16)},{getSprites(38,16,24,16)}
+                                                    };
+    public static BufferedImage PlayerSprite=getSprites(62,0,22,16);
+    public static BufferedImage BlockerSprite=getSprites(84,8,36,24);
+    public static BufferedImage getSprites(int x, int y, int width, int height){
+        BufferedImage spriteSheet=null;
+        BufferedImage sprite;
+        try{
+            spriteSheet= ImageIO.read(new File("invaders.png"));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        sprite=spriteSheet.getSubimage(x,y,width,height) ;
+        return sprite;
+    }
 }
